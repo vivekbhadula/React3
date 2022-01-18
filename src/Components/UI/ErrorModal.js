68107a1ae7be72b79277a73d8056478c4dd564e6
Vivek Bhadula <vivekbhadula201420@gmail.com>
2022-01-18 23:58:14 +0530

    added modal, complete

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -5,18 +5,21 @@ import Button from "./Button";
 
 function ErrorModal(props) {
   return (
-      <Card>
-          <header>
-              <h2></h2>
-          </header>
-          <div>
-              <p></p>
-          </div>
-          <footer>
-              <Button>Okay</Button>
-          </footer>
+    <div>
+      <div className={styles.backdrop} onClick={props.onConfirm}/>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
       </Card>
-  )
+    </div>
+  );
 }
 
 export default ErrorModal;
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   function addUserHandler(event) {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      console.log("error");
+      setError({
+        title: "Invalid Input",
+        message: "Enter a valid input. (Name or Age empty)",
+      });
       return;
     }
     if (+enteredAge < 1) {
-      console.log("error");
+      setError({
+        title: "Invalid age",
+        message: "Enter age greater than 0",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -30,26 +38,39 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   }
 
+  function errorHandler() {
+    setError(null);
+  }
+
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        ></input>
-        <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 }
 
